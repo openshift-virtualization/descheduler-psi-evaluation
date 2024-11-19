a90933f23380579865a33ab9358ec8bb1f7efa9d
Fabian Deutsch <fabiand@fedoraproject.org>
2024-11-19 11:00:04 +0100

    Add some testing boilerplate
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

---
+++ e2e-test.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+#
+set -e
+
+WITH_DEPLOY=${WITH_DEPLOY:-true}
+DRY=${DRY:-false}
+
+c() { echo "# $@" ; }
+n() { echo "" ; }
+x() { echo "\$ $@" ; ${DRY} || eval "$@" ; }
+red() { echo -e "\e[0;31m$@\e[0m" ; }
+green() { echo -e "\e[0;32m$@\e[0m" ; }
+die() { red "FATAL: $@" ; exit 1 ; }
+assert() { echo "(assert:) \$ $@" ; { ${DRY} || eval $@ ; } || { echo "(assert?) FALSE" ; die "Assertion ret 0 failed: '$@'" ; } ; green "(assert?) True" ; }
+
+c "Assumption: 'oc' is present and has access to the cluster"
+assert "which oc"
+
+
+c "# Reconfigure node-exporter to export PSI"
+
+c "Ensure that all MCP workers are updated"
+assert "oc get mcp worker -o json | jq -e '.status.conditions[] | select(.type == \"Updated\" and .status == \"True\")'"
+
+n
+c "Apply MachineConfig"
+x "bash to.sh deploy"
+
+n
+c "Wait for MCP to pickup new MC"
+x "bash to.sh wait_for_mcp"
+
+c "FIXME do some testing"
+
+n
+c "Delete the operator"
+x "bash to.sh destroy"
+x "bash to.sh wait_for_mcp"
+
+n
+c "Check the absence of swap"
+assert "bash to.sh check_nodes | grep -E '0\\s+0\\s+0'"
+
+n
+c "The validation has passed! All is well."
+
+green "PASS"
---
+++ to.sh
@@ -0,0 +1,37 @@
+i() { echo "i: $@"; }
+x() { echo "\$ $@" ; eval "$@" ; }
+die() { echo "err: $@" ; exit 1; }
+_oc() { echo "$ oc $@" ; oc $@ ; }
+qoc() { oc $@ > /dev/null 2>&1; }
+
+SA=descheduler
+NS=openshift-kube-descheduler-operator
+
+apply() {
+  _oc apply -f manifests/mc-psi.yaml
+  _oc apply -f manifests/descheduler-operator-cr.yaml
+}
+
+
+deploy() {
+  qoc get sa -n $NS $SA || die "Did not find descheduler ServiceAccount '$SA' in namespace '$NS'. Is it installed?"
+  _oc adm policy add-cluster-role-to-user cluster-monitoring-view -z $SA
+  apply
+}
+
+destroy() {
+  _oc delete -f manifests/mc-psi.yaml
+  _oc delete -f manifests/descheduler-operator-cr.yaml
+}
+
+
+wait_for_mcp() {
+  x "oc wait mcp worker --for condition=Updated=False --timeout=10s"
+  x "oc wait mcp worker --for condition=Updated=True --timeout=15m"
+}
+
+usage() {
+  grep -E -o "^.*\(\)" $0
+}
+
+eval "${@:-usage}"
