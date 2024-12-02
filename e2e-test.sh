bdfab42b4db48abdbd91482fc49b730f8219d7ac
Fabian Deutsch <fabiand@fedoraproject.org>
2024-12-02 16:42:58 +0100

    tests: Include workloads
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- e2e-test.sh
+++ e2e-test.sh
@@ -17,38 +17,40 @@ assert() { echo "(assert:) \$ $@" ; { ${DRY} || eval $@ ; } || { echo "(assert?)
 c "Assumption: 'oc' is present and has access to the cluster"
 assert "which oc"
 
+if $WITH_DEPLOY;
+then
+  c "# Reconfigure node-exporter to export PSI"
 
-c "# Reconfigure node-exporter to export PSI"
+  c "Ensure that all MCP workers are updated"
+  assert "oc get mcp worker -o json | jq -e '.status.conditions[] | select(.type == \"Updated\" and .status == \"True\")'"
 
-c "Ensure that all MCP workers are updated"
-assert "oc get mcp worker -o json | jq -e '.status.conditions[] | select(.type == \"Updated\" and .status == \"True\")'"
+  n
+  c "Apply MachineConfig"
+  x "bash to.sh deploy"
 
-n
-c "Apply MachineConfig"
-x "bash to.sh deploy"
-
-n
-c "Wait for MCP to pickup new MC"
-x "bash to.sh wait_for_mcp"
+  n
+  c "Wait for MCP to pickup new MC"
+  x "bash to.sh wait_for_mcp"
+fi
 
 n
 c "Create workloads"
 x "oc apply -f tests/00-vms-no-load.yaml -f tests/01-vms-cpu-load.yaml"
-c "oc wait --for jsonpath='.status.replicas'=3"
+c "oc wait --for jsonpath='.status.readyReplicas'=3 vmpool no-load"
+c "oc wait --for jsonpath='.status.readyReplicas'=3 vmpool cpu-load"
 
 TBD "wait for load and rebealance"
 
 c "Delete workloads"
 x "oc delete -f tests/00-vms-no-load.yaml -f tests/01-vms-cpu-load.yaml"
 
-n
-c "Delete the operator"
-x "bash to.sh destroy"
-x "bash to.sh wait_for_mcp"
-
-n
-c "Check the absence of swap"
-assert "bash to.sh check_nodes | grep -E '0\\s+0\\s+0'"
+if $WITH_DEPLOY;
+then
+  n
+  c "Delete the operator"
+  x "bash to.sh destroy"
+  x "bash to.sh wait_for_mcp"
+fi
 
 n
 c "The validation has passed! All is well."
