5cf766d807cf85ce6b6d46dd0a52169207e75fbe
Fabian Deutsch <fabiand@fedoraproject.org>
2024-12-04 11:05:03 +0100

    to: Add operator deployment and installation
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- to.sh
+++ to.sh
@@ -11,27 +11,31 @@ apply() {
   _oc apply -f manifests/10-mc-psi-controlplane.yaml
   _oc apply -f manifests/11-mc-psi-worker.yaml
   _oc apply -f manifests/20-namespaces.yaml
-  _oc apply -f manifests/30-subscriptions.yaml
-  until _oc get crd hyperconverged ; do sleep 6 ; done
+  _oc apply -f manifests/30-operatorgroup.yaml
+  _oc apply -f manifests/31-subscriptions.yaml
+  until _oc get crd hyperconvergeds.hco.kubevirt.io 2> /dev/null ; do echo . ; sleep 6 ; done
+  until _oc get crd kubedeschedulers.operator.openshift.io 2> /dev/null ; do echo . ; sleep 6 ; done
   _oc apply -f manifests/40-cnv-operator-cr.yaml
   _oc apply -f manifests/40-descheduler-operator-cr.yaml
 }
 
 
 deploy() {
-  qoc get sa -n $NS $SA || die "Did not find descheduler ServiceAccount '$SA' in namespace '$NS'. Is it installed?"
-  _oc adm policy add-cluster-role-to-user cluster-monitoring-view -z $SA -n $NS
   apply
   wait_for_mcp
+  qoc get sa -n $NS $SA || die "Did not find descheduler ServiceAccount '$SA' in namespace '$NS'. Is it installed?"
+  _oc adm policy add-cluster-role-to-user cluster-monitoring-view -z $SA -n $NS
+
 }
 
 destroy() {
-  _oc apply -f manifests/10-mc-psi-controlplane.yaml
-  _oc apply -f manifests/11-mc-psi-worker.yaml
-  _oc apply -f manifests/20-namespaces.yaml
-  _oc apply -f manifests/30-subscriptions.yaml
-  _oc apply -f manifests/40-cnv-operator-cr.yaml
-  _oc apply -f manifests/40-descheduler-operator-cr.yaml
+  _oc delete -f manifests/40-descheduler-operator-cr.yaml
+  _oc delete -f manifests/40-cnv-operator-cr.yaml
+  _oc delete -f manifests/31-subscriptions.yaml
+  _oc delete -f manifests/30-operatorgroup.yaml
+  _oc delete -f manifests/20-namespaces.yaml
+#  _oc delete -f manifests/11-mc-psi-worker.yaml
+#  _oc delete -f manifests/10-mc-psi-controlplane.yaml
 }
 
 
